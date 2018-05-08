6f597f7b941dfb06f501f2293582c3964cb86e3a
zhangze <zhangze@wanbu.com.cn>
2018-05-08 20:22:30 +0800

    项目初始化
    主项目初始化

---
+++ BeWorth/BeWorth/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BeWorth
+//
+//  Created by yaoln on 2018/5/8.
+//  Copyright © 2018年 zz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ BeWorth/BeWorth/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  BeWorth
+//
+//  Created by yaoln on 2018/5/8.
+//  Copyright © 2018年 zz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
